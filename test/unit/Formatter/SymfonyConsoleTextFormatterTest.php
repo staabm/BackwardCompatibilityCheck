eac84fde931b9ac1541e2667048b1af7ee7773db
Luís Cobucci <lcobucci@gmail.com>
2023-03-02 01:14:23 +0100

    Move away from deprecated MockObject#withConsecutive()
    More info: https://github.com/sebastianbergmann/phpunit/issues/5063
    
    Signed-off-by: Luís Cobucci <lcobucci@gmail.com>

--- test/unit/Formatter/SymfonyConsoleTextFormatterTest.php
+++ test/unit/Formatter/SymfonyConsoleTextFormatterTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace RoaveTest\BackwardCompatibility\Formatter;
 
-use ArrayIterator;
 use PHPUnit\Framework\TestCase;
 use Psl\SecureRandom;
 use Psl\Str;
@@ -12,7 +11,9 @@ use ReflectionException;
 use Roave\BackwardCompatibility\Change;
 use Roave\BackwardCompatibility\Changes;
 use Roave\BackwardCompatibility\Formatter\SymfonyConsoleTextFormatter;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+use const PHP_EOL;
 
 /** @covers \Roave\BackwardCompatibility\Formatter\SymfonyConsoleTextFormatter */
 final class SymfonyConsoleTextFormatterTest extends TestCase
@@ -23,26 +24,17 @@ final class SymfonyConsoleTextFormatterTest extends TestCase
         $change1Text = SecureRandom\string(8);
         $change2Text = SecureRandom\string(8);
 
-        $output = $this->createMock(OutputInterface::class);
-
-        $expectedMessages = new ArrayIterator([
-            Str\format('[BC] REMOVED: %s', $change1Text),
-            Str\format('     ADDED: %s', $change2Text),
-        ]);
-
-        $output->expects(self::exactly(2))
-            ->method('writeln')
-            ->willReturnCallback(static function (string $text) use ($expectedMessages): void {
-                self::assertTrue($expectedMessages->valid());
-                $expectedText = $expectedMessages->current();
-                $expectedMessages->next();
-
-                self::assertSame($expectedText, $text);
-            });
+        $output = new BufferedOutput();
 
         (new SymfonyConsoleTextFormatter($output))->write(Changes::fromList(
             Change::removed($change1Text, true),
             Change::added($change2Text, false),
         ));
+
+        self::assertSame(
+            Str\format('[BC] REMOVED: %s', $change1Text) . PHP_EOL .
+            Str\format('     ADDED: %s', $change2Text) . PHP_EOL,
+            $output->fetch(),
+        );
     }
 }
