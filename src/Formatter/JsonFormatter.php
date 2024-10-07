1ae1e627e0f4ae94dfac6bd6102b474d29b1ec6a
Tobias Nyholm <tobias.nyholm@gmail.com>
2024-10-07 17:17:07 +0200

    Add JsonFormatter
    This is useful when other programs wants to read the output data

---
+++ src/Formatter/JsonFormatter.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\BackwardCompatibility\Formatter;
+
+use Psl\Str;
+use Roave\BackwardCompatibility\Changes;
+use Roave\BackwardCompatibility\Git\CheckedOutRepository;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use function json_encode;
+
+/** @internal */
+final class JsonFormatter implements OutputFormatter
+{
+    public function __construct(
+        private readonly OutputInterface $output,
+        private CheckedOutRepository $basePath,
+    ) {
+    }
+
+    public function write(Changes $changes): void
+    {
+        $basePath = $this->basePath->__toString() . '/';
+        $result   = [];
+
+        foreach ($changes as $change) {
+            $result[] = [
+                'description' => $change->description,
+                'path' => $change->file === null ? null : Str\replace($change->file, $basePath, ''),
+                'line' => $change->line,
+                'column' => $change->column,
+            ];
+        }
+
+        $this->output->writeln(json_encode(['errors' => $result]));
+    }
+}
